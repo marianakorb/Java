95466cccd1c37f556b43cb0e50a2f543e125bd30
marianakorb <74978652+marianakorb@users.noreply.github.com>
2023-06-16 14:26:33 -0300

    aula06 poo

---
+++ Aula06/src/aula06/Encapsulamento.java
@@ -0,0 +1,14 @@
+package aula06;
+
+public class Encapsulamento {
+
+	public static void main(String[] args) {
+		
+		ControleRemoto controle = new ControleRemoto();
+		
+		controle.ligar();
+		controle.maisVolume();
+		controle.abrirMenu();
+	}
+
+}
