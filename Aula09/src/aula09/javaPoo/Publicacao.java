230b27135fdb5810cbfbe91eaea327ec35bd7a6a
marianakorb <74978652+marianakorb@users.noreply.github.com>
2023-06-16 14:27:34 -0300

    aula09 poo

---
+++ Aula09/src/aula09/javaPoo/Publicacao.java
@@ -0,0 +1,16 @@
+package aula09.javaPoo;
+
+public interface Publicacao {
+	
+	public void abrir();
+	
+	public void fechar(); 	
+	
+	public void folhear(int p); 		
+		
+	public void avancarPag(); 	
+		
+	public void voltarPag(); 
+		
+	
+}
